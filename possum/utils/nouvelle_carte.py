1a5ca5083f9388b7bdc808455966d574aff100c3
sbonnegent <sebastien.bonnegent@gmail.com>
2013-09-18 10:22:21 +0200

    saisie nouvelle carte

---
+++ possum/utils/nouvelle_carte.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2009-2013 Sébastien Bonnegent
+#
+#    This file is part of POSSUM.
+#
+#    POSSUM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    POSSUM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with POSSUM.  If not, see <http://www.gnu.org/licenses/>.
+#
+import sys, os
+sys.path.append('/home/pos')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'possum.settings'
+
+from possum.base.models import Accompagnement, Sauce, Etat, \
+    Categorie, Couleur, Cuisson, Facture, Log, LogType, Paiement, \
+    PaiementType, Produit, ProduitVendu, Suivi, Table, Zone
+
+# on efface tous les produits présents
+Produit.objects.all().delete()
+Categorie.objects.all().delete()
+
+# on entre les nouveaux produits
+cat = Categorie(nom="Entrees2")
+cat.save()
+Produit(nom="salade", nom_facture="salade", prix="3.40", categorie=cat)
+Produit(nom="salade2", nom_facture="salade2", prix="3.40", categorie=cat)
+
