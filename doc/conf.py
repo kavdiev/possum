4cc4ac9821330e015226febc2e55f0e75b65da3b
sbonnegent <sebastien.bonnegent@gmail.com>
2013-09-29 10:12:18 +0200

    favicon.ico pour sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -135,7 +135,8 @@ html_logo = "../possum/static/images/bandeau-192.png"
 # pixels large.
 #html_favicon = None
 #html_favicon = "../possum/images/favicon.ico"
-html_favicon = "../possum/static/images/favicon.png"
+#html_favicon = "../possum/static/images/favicon.png"
+html_favicon = "../possum/static/images/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
