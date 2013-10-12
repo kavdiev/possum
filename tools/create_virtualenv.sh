d9dea421efba4f2d9c083dc28fb76d8c1d29d5f1
Pierre <pierre.sassoulas@gmail.com>
2013-10-12 23:54:30 +0200

    Précision pour l'installation

--- tools/create_virtualenv.sh
+++ tools/create_virtualenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 virtualenv .virtualenv
 echo "Launch your virtualenv with : 'source .virtualenv/bin/activate'"
+echo "Then use .install_virtualenv.sh (You must have graphviz-dev and libcups2-dev)"
 
