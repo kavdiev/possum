bdb965235aa1249176752f9b71858d465fb25cf9
sbonnegent <sebastien.bonnegent@gmail.com>
2013-10-13 15:04:00 +0200

    We stay with python2

--- tools/create_virtualenv.sh
+++ tools/create_virtualenv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-virtualenv .virtualenv
+# For the moment, we stay with python2.
+# Must be execute like this: tools/create_virtualenv.sh
+virtualenv --python=python2 .virtualenv
 echo "Launch your virtualenv with : 'source .virtualenv/bin/activate'"
 
