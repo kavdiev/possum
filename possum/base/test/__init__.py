6a84b0db07559baa88fbe3148be3171c0c18a120
sbonnegent <sebastien.bonnegent@gmail.com>
2013-11-13 16:09:42 +0100

    Tests

--- possum/base/test/__init__.py
+++ possum/base/test/__init__.py
@@ -5,6 +5,6 @@
 #from test_monthlystat import Test_MonthlyStat
 #from test_printer import Test_Printer
 #from test_product import Test_Produit, Test_ProduitVendu
-from test_vat import Test_VAT
+#from test_vat import Test_VAT
 #from test_weeklystat import Test_WeeklyStat
 #from test_views import Test_Views
