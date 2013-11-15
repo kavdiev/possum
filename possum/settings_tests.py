b9f6793cfbf397a904df2903dfbdcb872fb8c95c
sbonnegent <sebastien.bonnegent@gmail.com>
2013-11-15 16:47:01 +0100

    flake8

--- possum/settings_tests.py
+++ possum/settings_tests.py
@@ -53,7 +53,7 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.dir_tests',
     'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_pyflakes',
+#    'django_jenkins.tasks.run_pyflakes',
 )
 
 #COVERAGE_EXCLUDES = ['django_extensions', 'django', '*migrations*', '.virtualenv']
