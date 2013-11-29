350ac77304fd33cb76aa67f6cbee155cd0a07ee7
sbonnegent <sebastien.bonnegent@gmail.com>
2013-11-29 17:19:47 +0100

    pep8

--- possum/base/views/manager/tables/__init__.py
+++ possum/base/views/manager/tables/__init__.py
@@ -18,55 +18,33 @@
 #    along with POSSUM.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import datetime
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.context_processors import PermWrapper
-from django.contrib.auth.models import User, UserManager, Permission
-from django.core.mail import send_mail
-from django.http import Http404, HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 import logging
-import os
-
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.utils.functional import wraps
-
-from possum.base.bill import Facture
-from possum.base.category import Categorie
-from possum.base.forms import DateForm, WeekForm, MonthForm, YearForm
 from possum.base.location import Zone, Table
-from possum.base.models import Printer
-from possum.base.options import Cuisson, Sauce, Accompagnement
-from possum.base.payment import PaiementType, Paiement
-from possum.base.product import Produit, ProduitVendu
-from possum.base.vat import VAT
 from possum.base.views import get_user, permission_required
 
 
 logger = logging.getLogger(__name__)
 
 
-# from django.views.decorators.csrf import csrf_protect
-
 @permission_required('base.p1')
 def tables_zone_delete(request, zone_id):
-    data = get_user(request)
     zone = get_object_or_404(Zone, pk=zone_id)
     Table.objects.filter(zone=zone).delete()
     zone.delete()
     return HttpResponseRedirect('/manager/tables/')
 
+
 @permission_required('base.p1')
 def tables_table_new(request, zone_id):
-    data = get_user(request)
     zone = get_object_or_404(Zone, pk=zone_id)
     table = Table(zone=zone)
     table.save()
     return HttpResponseRedirect('/manager/tables/%s/%s/' % (zone.id, table.id))
+
 
 @permission_required('base.p1')
 def tables_zone_new(request):
@@ -74,6 +52,7 @@ def tables_zone_new(request):
     zone = Zone()
     zone.save()
     return HttpResponseRedirect('/manager/tables/%s/' % zone.id)
+
 
 @permission_required('base.p1')
 def tables_table(request, zone_id, table_id):
@@ -93,6 +72,7 @@ def tables_table(request, zone_id, table_id):
     return render_to_response('base/manager/tables/table.html', data,
                               context_instance=RequestContext(request))
 
+
 @permission_required('base.p1')
 def tables_zone(request, zone_id):
     data = get_user(request)
@@ -110,6 +90,7 @@ def tables_zone(request, zone_id):
             return HttpResponseRedirect('/manager/tables/')
     return render_to_response('base/manager/tables/zone.html', data,
                               context_instance=RequestContext(request))
+
 
 @permission_required('base.p1')
 def tables(request):
