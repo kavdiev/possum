2cfdb92c81ec1cdba9dd19937d11621e1f807bb4
sbonnegent <sebastien.bonnegent@gmail.com>
2013-12-07 00:31:45 +0100

    tests

--- possum/settings_tests.py
+++ possum/settings_tests.py
@@ -52,7 +52,7 @@ JENKINS_TASKS = (
 #    'django_jenkins.tasks.run_pylint',
 #    'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.dir_tests',
+#    'django_jenkins.tasks.dir_tests',
 #    'django_jenkins.tasks.run_csslint',
 #    'django_jenkins.tasks.run_pyflakes',
 )
