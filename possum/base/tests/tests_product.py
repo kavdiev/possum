0d77598dd9ec827ef3d128b1d90cfb490149345a
sbonnegent <sebastien.bonnegent@gmail.com>
2013-12-07 12:27:21 +0100

    tests

--- possum/base/tests/tests_product.py
+++ possum/base/tests/tests_product.py
@@ -14,44 +14,44 @@ class Tests_Products(TestCase):
 
     def test_is_full(self):
         menu = ProduitVendu()
-        menu.produit = Produit.object.get(nom="biere 50cl")
+        menu.produit = Produit.objects.get(nom="biere 50cl")
         menu.save()
         self.assertTrue(menu.isFull())
 
-        menu.produit = Produit.object.get(nom="Entree/Plat")
+        menu.produit = Produit.objects.get(nom="Entree/Plat")
         self.assertFalse(menu.isFull())
 
         plat = ProduitVendu()
-        plat.produit = Produit.object.get(nom="entrecote")
+        plat.produit = Produit.objects.get(nom="entrecote")
         plat.save()
         menu.produits.add(plat)
         self.assertFalse(menu.isFull())
 
         entree = ProduitVendu()
-        entree.produit = Produit.object.get(nom="salade normande")
+        entree.produit = Produit.objects.get(nom="salade normande")
         entree.save()
         menu.produits.add(entree)
         self.assertTrue(menu.isFull())
 
     def test_free_category(self):
         menu = ProduitVendu()
-        menu.produit = Produit.object.get(nom="biere 50cl")
+        menu.produit = Produit.objects.get(nom="biere 50cl")
         menu.save()
         self.assertEqual(None, menu.getFreeCategorie())
 
-        menu.produit = Produit.object.get(nom="Entree/Plat")
+        menu.produit = Produit.objects.get(nom="Entree/Plat")
         cat_entrees = Categorie.objects.get(nom="Entrees")
         self.assertEqual(cat_entrees, menu.getFreeCategorie())
 
         entree = ProduitVendu()
-        entree.produit = Produit.object.get(nom="salade normande")
+        entree.produit = Produit.objects.get(nom="salade normande")
         entree.save()
         menu.produits.add(entree)
         cat_plats = Categorie.objects.get(nom="Plat")
         self.assertEqual(cat_plats, menu.getFreeCategorie())
 
         plat = ProduitVendu()
-        plat.produit = Produit.object.get(nom="entrecote")
+        plat.produit = Produit.objects.get(nom="entrecote")
         plat.save()
         menu.produits.add(plat)
         self.assertEqual(None, menu.getFreeCategorie())
