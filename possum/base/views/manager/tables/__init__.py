65d1f94eff68ef3efe8793f02030030cf63f0a2a
sbonnegent <sebastien.bonnegent@gmail.com>
2013-12-17 10:27:53 +0100

    affiche la version utilisée

--- possum/base/views/manager/tables/__init__.py
+++ possum/base/views/manager/tables/__init__.py
@@ -23,7 +23,7 @@ from django.http import HttpResponseRedirect
 import logging
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from possum.base.location import Zone, Table
+from possum.base.models import Zone, Table
 from possum.base.views import get_user, permission_required
 
 
