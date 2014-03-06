5d256ed3610193567918b7385fc79209a57b72ad
sbonnegent <sebastien.bonnegent@gmail.com>
2014-03-06 18:15:14 +0100

    [doc] create settings.py if needed

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,13 @@ import os
 import sys
 
 
+# if needed, create possum/settings.py
+CONF = os.path.join("..", "possum", "settings.py")
+CONF_TEMPLATE = os.path.join("..", "possum", "settings_production.py")
+if not os.path.isfile(CONF):
+    import shutil
+    shutil.copyfile(CONF_TEMPLATE, CONF)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
